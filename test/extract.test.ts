ed69853fadc4bc08b3e75d54342522799d87dab9
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-02-03 11:59:06 +0300

    feat: add vitest

--- test/extract.test.ts
+++ test/extract.test.ts
@@ -1,4 +1,12 @@
-import { extract } from "../src/index";
+import { extract } from "../src/parser";
+import { test, expect } from "vitest";
 
-const json = extract("./extractTest.md")
-console.log(json)
+const correctResult = [
+    '{"link":"https://devxconf.org/","Name":"DevX Conf","Location":"Online","Date":"4/28/2021"}'
+]
+
+test("extract method", async () => {
+    const filePath = "./test/extractTest.md";
+    const result = await extract(filePath);
+    expect(result).toStrictEqual(correctResult);
+});
